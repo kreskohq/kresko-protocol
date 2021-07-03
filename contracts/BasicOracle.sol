a1e3ef89063f3b778e6cf952c1103719f3f748d0
Alex Harley <alexbruceharley@gmail.com>
2021-07-03 15:28:39 +0200

    feat: add BasicOracle contract

---
+++ contracts/BasicOracle.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BasicOracle is Ownable {
+    address public reporter;
+    uint256 public value;
+
+    event SetReporter(address oracle);
+
+    modifier onlyReporter() {
+        require(msg.sender == reporter, "Sender not oracle");
+        _;
+    }
+
+    constructor(address reporter_) {
+        _setReporter(reporter_);
+    }
+
+    function setValue(uint256 newValue) external onlyReporter {
+        value = newValue;
+    }
+
+    function setReporter(address newReporter) external onlyOwner {
+        _setReporter(newReporter);
+    }
+
+    function _setReporter(address newReporter) internal {
+        reporter = newReporter;
+        emit SetReporter(newReporter);
+    }
+}
