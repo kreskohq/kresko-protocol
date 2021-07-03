a75e490fcf2e71f004287853e2a78875a551fb4f
Alex Harley <alexbruceharley@gmail.com>
2021-07-03 13:54:30 +0200

    chore: cleanup hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,29 +14,25 @@ import { NetworkUserConfig } from "hardhat/types";
 
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
-const chainIds = {
-  ganache: 1337,
-  goerli: 5,
-  hardhat: 31337,
-  kovan: 42,
-  mainnet: 1,
-  rinkeby: 4,
-  ropsten: 3,
+const mnemonic = process.env.MNEMONIC;
+
+interface Network {
+  rpcUrl: string;
+  chainId: number;
+}
+
+const networks = {
+  alfajores: {
+    rpcUrl: "https://alfajores-forno.celo-testnet.org",
+    chainId: 44787,
+  },
+  mainnet: {
+    rpcUrl: "https://forno.celo.org",
+    chainId: 42220,
+  },
 };
 
-// Ensure that we have all the environment variables we need.
-const mnemonic = process.env.MNEMONIC;
-if (!mnemonic) {
-  throw new Error("Please set your MNEMONIC in a .env file");
-}
-
-const infuraApiKey = process.env.INFURA_API_KEY;
-if (!infuraApiKey) {
-  throw new Error("Please set your INFURA_API_KEY in a .env file");
-}
-
-function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
-  const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
+function createTestnetConfig(network: Network): NetworkUserConfig {
   return {
     accounts: {
       count: 10,
@@ -44,8 +40,8 @@ function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig 
       mnemonic,
       path: "m/44'/60'/0'/0",
     },
-    chainId: chainIds[network],
-    url,
+    chainId: network.chainId,
+    url: network.rpcUrl,
   };
 }
 
@@ -62,12 +58,10 @@ const config: HardhatUserConfig = {
       accounts: {
         mnemonic,
       },
-      chainId: chainIds.hardhat,
+      chainId: 31337,
     },
-    goerli: createTestnetConfig("goerli"),
-    kovan: createTestnetConfig("kovan"),
-    rinkeby: createTestnetConfig("rinkeby"),
-    ropsten: createTestnetConfig("ropsten"),
+    alfajores: createTestnetConfig(networks.alfajores),
+    mainnet: createTestnetConfig(networks.mainnet),
   },
   paths: {
     artifacts: "./artifacts",
