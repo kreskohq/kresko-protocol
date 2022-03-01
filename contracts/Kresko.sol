f1af5173de4ee48e745076ebca09ec46babd27a9
panukettu <panu.t.kettunen@gmail.com>
2022-03-01 05:15:57 +0200

    fix: add comments

--- contracts/Kresko.sol
+++ contracts/Kresko.sol
@@ -1429,17 +1429,28 @@ contract Kresko is OwnableUpgradeable, ReentrancyGuardUpgradeable {
             }
 
             // Cascade the liquidations if user has multiple assets.
+            // This is desired because pairs with low cFactor and high kFactor have a
+            // higher collateral requirement than positions with high cFactor and low kFactor.
+
+            // Main reason here is keep the liquidations from happening only on pairs that have a high risk profile.
             if (mintedKreskoAssets[_account].length + depositedCollateralAssets[_account].length > 2) {
-                // This is desired because assets with lower cFactor and higher kFactor have a higher solvency margin
-                // than positions with higher cFactor and lower kFactor.
+                // A liquidator can abuse this by only liquidating high risk positions
+                // gaining way more of the users collateral than required if the user has a lower risk pair available.
 
-                // The side effect here is lowwer the cFactor, the higher the amount of liquidations needed
-                // to ensure liquidations do not only target the riskiest pairs.
+                // To mitigate this practically to zero:
+                // cFactor^4 the collateral available (cFactor = 1 == nothing happens)
+                // Return:
+                // Get the ratio between max liquidatable USD and diminished collateral available
+                // = (higher value -> higher the risk ratio of this pair)
+                // Divide the maxValue by this ratio and a diminishing max value is returned.
 
-                // Include a burnFee surplus in the liquidation
-                // so the users can repay their debt.
+                // For a max profit liquidation strategy jumps to other pairs must happen before
+                // the liquidation value of the risky position becomes the most profitable again.
+
                 return
                     maxLiquidatableUSD.div(maxLiquidatableUSD.div(collateralValueAvailable.mul(cFactor.pow(4)))).mul(
+                        // Include a burnFee surplus in the liquidation
+                        // so the users can repay their debt.
                         FixedPoint.Unsigned(ONE_HUNDRED_PERCENT).add(burnFee.mul(1))
                     );
             } else {
