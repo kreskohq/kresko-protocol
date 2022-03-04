266de0cbd1b53640a099a52b2267d5464f6dbfeb
panukettu <panu.t.kettunen@gmail.com>
2022-03-04 18:55:22 +0200

    fix: add kr asset task

--- src/tasks/kresko/addKrAsset.ts
+++ src/tasks/kresko/addKrAsset.ts
@@ -6,7 +6,7 @@ task("kresko:addkrasset")
     .addParam("name", "Name of the asset")
     .addParam("kFactor", "kFactor for the asset", 1000, types.float)
     .addParam("oracleAddr", "Price feed address")
-    .addParam("marketCapLimit", "Market cap USD limit")
+    .addParam("marketCapLimit", "Market cap USD limit", 10_000_000, types.int)
     .addOptionalParam("log", "Log outputs", false, types.boolean)
     .addOptionalParam("wait", "Log outputs", 1, types.int)
     .setAction(async function (taskArgs: TaskArguments, hre) {
