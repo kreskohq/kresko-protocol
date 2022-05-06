d805c3dfe3dac6c9d22f51d072b854c1cf5b1dcf
diofinance <diofinance@protonmail.com>
2022-05-06 12:24:10 +0200

    price aggregation tests

--- src/test/flux/priceaggregator/FluxPriceAggregator.behavior.ts
+++ src/test/flux/priceaggregator/FluxPriceAggregator.behavior.ts
@@ -1,20 +1,106 @@
 import { expect } from "chai";
 
 export function shouldBehaveLikeFluxPriceAggregator(): void {
-    it("should aggregate latest prices from oracles", async function () {
+    it("should aggregate latest prices from 1 oracle, ignoring uninitialized oracles", async function () {
         await this.oracles[0].connect(this.signers.admin).transmit(100, true);
-        await this.oracles[1].connect(this.signers.admin).transmit(150, true);
-        await this.oracles[2].connect(this.signers.admin).transmit(300, true);
         expect(await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).to.equal(0);
+
         await this.priceaggregator.connect(this.signers.admin).updatePrices();
-        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(183);
+
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(100);
         expect((await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).toNumber()).to.greaterThan(0);
     });
-    it("should count uninitialized prices as zero", async function () {
+    it("should aggregate latest prices from 2 oracles, ignoring uninitialized oracles", async function () {
         await this.oracles[0].connect(this.signers.admin).transmit(100, true);
-        await this.oracles[1].connect(this.signers.admin).transmit(75, true);
+        await this.oracles[2].connect(this.signers.admin).transmit(300, true);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).to.equal(0);
+
         await this.priceaggregator.connect(this.signers.admin).updatePrices();
-        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(58);
+
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(200);
+        expect((await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).toNumber()).to.greaterThan(0);
+    });
+    it("should aggregate latest prices from 3+ oracles", async function () {
+        await this.oracles[0].connect(this.signers.admin).transmit(100, true);
+        await this.oracles[1].connect(this.signers.admin).transmit(125, true);
+        await this.oracles[2].connect(this.signers.admin).transmit(300, true);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).to.equal(0);
+
+        await this.priceaggregator.connect(this.signers.admin).updatePrices();
+
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(125);
+        expect((await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).toNumber()).to.greaterThan(0);
+    });
+    it("should determine market open/closed from 1 oracle, ignoring uninitialized oracles", async function () {
+        await this.oracles[0].connect(this.signers.admin).transmit(100, true);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).to.equal(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(false);
+
+        await this.priceaggregator.connect(this.signers.admin).updatePrices();
+
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(100);
+        expect((await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).toNumber()).to.greaterThan(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(true);
+    });
+    it("should determine market open/closed from 2 oracles, ignoring uninitialized oracles", async function () {
+        await this.oracles[0].connect(this.signers.admin).transmit(100, true);
+        await this.oracles[1].connect(this.signers.admin).transmit(150, true);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).to.equal(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(false);
+
+        await this.priceaggregator.connect(this.signers.admin).updatePrices();
+
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(125);
+        expect((await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).toNumber()).to.greaterThan(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(true);
+    });
+    it("should evaluate split 50-50 decision on market as market closed, ignoring uninitialized oracles", async function () {
+        await this.oracles[0].connect(this.signers.admin).transmit(100, true);
+        await this.oracles[1].connect(this.signers.admin).transmit(150, false);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).to.equal(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(false);
+
+        await this.priceaggregator.connect(this.signers.admin).updatePrices();
+
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(125);
+        expect((await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).toNumber()).to.greaterThan(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(false);
+    });
+    it("should determine market open/closed from 3+ oracles", async function () {
+        // Consider situation:
+        //      - Price changes 20% overnight and on open majority of oracles post prices and updatesPrices is called.
+        //      - The market is considered open, but yesterday's closing price from oracles that haven't posted today
+        //        yet is still considered in the median calculation.
+        //      
+        //      Oracles at close:                       [a, b, c, d, e]
+        //      Closing prices:                         [100, 100, 100, 100, 100], [closed, closed, closed, closed, closed]
+        //      At open when update prices is called:   [100, 100, 120, 120, 121], [closed, closed, open, open, open]
+        //
+        //      Outcome is valid: market is open with price 120.
+
+        await this.oracles[0].connect(this.signers.admin).transmit(100, false);
+        await this.oracles[1].connect(this.signers.admin).transmit(120, true);
+        await this.oracles[2].connect(this.signers.admin).transmit(121, true);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).to.equal(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(false);
+
+        await this.priceaggregator.connect(this.signers.admin).updatePrices();
+
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(120);
+        expect((await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).toNumber()).to.greaterThan(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(true);
+    });
+    it("should ignore negative prices, not including them in price aggregation or market open/closed voting", async function () {
+        await this.oracles[0].connect(this.signers.admin).transmit(100, true);
+        await this.oracles[1].connect(this.signers.admin).transmit(-20, false);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).to.equal(0);
+
+        await this.priceaggregator.connect(this.signers.admin).updatePrices();
+
+        expect(await this.priceaggregator.connect(this.signers.admin).latestAnswer()).to.equal(100);
+        expect((await this.priceaggregator.connect(this.signers.admin).latestTimestamp()).toNumber()).to.greaterThan(0);
+        expect(await this.priceaggregator.connect(this.signers.admin).latestMarketOpen()).to.equal(true);
     });
     it("should allow deployer to change delay", async function () {
         await this.priceaggregator.connect(this.signers.admin).setDelay(12345);
