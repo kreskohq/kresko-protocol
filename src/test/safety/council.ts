1427121f28740d3cd055a7f53f610188a1695d57
diofinance <diofinance@protonmail.com>
2022-10-14 14:40:18 +0200

    multisig signature threshold

--- src/test/safety/council.ts
+++ src/test/safety/council.ts
@@ -12,13 +12,140 @@ describe.only("Council", function () {
         this.collateral = this.collaterals[0];
         this.krAsset = this.krAssets[0];
 
-        const { deployer, devTwo, extOne } = await hre.ethers.getNamedSigners();
+        // These are the 5 signers on the SafetyCouncil multisig
+        const { deployer, devTwo, extOne, extTwo, extThree } = await hre.ethers.getNamedSigners();
         this.deployer = deployer;
         this.devTwo = devTwo;
         this.extOne = extOne;
+        this.extTwo = extTwo;
+        this.extThree = extThree;
     });
    
     describe("#toggleAssetsPaused", () => {
+        describe("multisig signature threshold", () => {
+            it("multisig transacts successfully with majority of signers (3/5)", async function () {
+                await executeContractCallWithSigners(
+                    hre.Multisig,
+                    hre.Diamond,
+                    "toggleAssetsPaused",
+                    [[this.collateral.address], Action.DEPOSIT, true, 0],
+                    [this.deployer, this.devTwo, this.extOne],
+                );
+
+                const isPaused = await hre.Diamond.assetActionPaused(
+                    Action.DEPOSIT.toString(),
+                    this.collateral.address,
+                );
+                expect(isPaused).to.equal(true);
+            });
+
+            it("multisig transacts successfully with super-majority of signers (4/5)", async function () {
+                await executeContractCallWithSigners(
+                    hre.Multisig,
+                    hre.Diamond,
+                    "toggleAssetsPaused",
+                    [[this.collateral.address], Action.DEPOSIT, true, 0],
+                    [this.deployer, this.devTwo, this.extOne, this.extTwo],
+                );
+
+                const isPaused = await hre.Diamond.assetActionPaused(
+                    Action.DEPOSIT.toString(),
+                    this.collateral.address,
+                );
+                expect(isPaused).to.equal(true);
+            });
+
+            it("multisig transacts successfully with all signers (5/5)", async function () {
+                await executeContractCallWithSigners(
+                    hre.Multisig,
+                    hre.Diamond,
+                    "toggleAssetsPaused",
+                    [[this.collateral.address], Action.DEPOSIT, true, 0],
+                    [this.deployer, this.devTwo, this.extOne, this.extTwo, this.extThree],
+                );
+
+                const isPaused = await hre.Diamond.assetActionPaused(
+                    Action.DEPOSIT.toString(),
+                    this.collateral.address,
+                );
+                expect(isPaused).to.equal(true);
+            });
+
+            it("multisig should reject transactions signed by a minority of signers (2/5)", async function () {
+                await expect(executeContractCallWithSigners(
+                    hre.Multisig,
+                    hre.Diamond,
+                    "toggleAssetsPaused",
+                    [[this.collateral.address], Action.DEPOSIT, true, 0],
+                    [this.deployer, this.devTwo],
+                )).to.be.revertedWith("");
+
+                const isPaused = await hre.Diamond.assetActionPaused(
+                    Action.DEPOSIT.toString(),
+                    this.collateral.address,
+                );
+                expect(isPaused).to.equal(false);
+            });
+        });
+
+        describe("toggle actions only for listed assets", () => {
+            it("can toggle actions for listed collateral assets", async function () {
+                await executeContractCallWithSigners(
+                    hre.Multisig,
+                    hre.Diamond,
+                    "toggleAssetsPaused",
+                    [[this.collateral.address], Action.DEPOSIT, true, 0],
+                    [this.deployer, this.devTwo, this.extOne],
+                );
+
+                const isPaused = await hre.Diamond.assetActionPaused(
+                    Action.DEPOSIT.toString(),
+                    this.collateral.address,
+                );
+                expect(isPaused).to.equal(true);
+            });
+
+            it("can toggle actions for listed krAssets", async function () {
+                await executeContractCallWithSigners(
+                    hre.Multisig,
+                    hre.Diamond,
+                    "toggleAssetsPaused",
+                    [[this.krAsset.address], Action.REPAY, true, 0],
+                    [this.deployer, this.devTwo, this.extOne],
+                );
+
+                const isPaused = await hre.Diamond.assetActionPaused(
+                    Action.REPAY.toString(),
+                    this.krAsset.address,
+                );
+                expect(isPaused).to.equal(true);
+            });
+
+            it("cannot toggle actions for addresses that are not listed collateral assets or krAssets", async function () {
+                const randomAddr = hre.ethers.utils.computeAddress("0xb976778317b23a1285ec2d483eda6904d9319135b89f1d8eee9f6d2593e2665d");
+
+                await expect(executeContractCallWithSigners(
+                    hre.Multisig,
+                    hre.Diamond,
+                    "toggleAssetsPaused",
+                    [[randomAddr], Action.DEPOSIT, true, 0],
+                    [this.deployer, this.devTwo, this.extOne],
+                )).to.be.revertedWith("");
+
+                const isPaused = await hre.Diamond.assetActionPaused(
+                    Action.DEPOSIT.toString(),
+                    randomAddr,
+                );
+                expect(isPaused).to.equal(false);
+            });
+        });
+
+        describe("time", () => {
+            it("", async function () {
+                // TODO:
+            });
+        });
+
         describe("toggle all possible actions: DEPOSIT, WITHDRAW, REPAY, BORROW, LIQUIDATION", () => {
             it("can toggle action DEPOSIT pause status on and off", async function () {
                 await executeContractCallWithSigners(
@@ -166,58 +293,6 @@ describe.only("Council", function () {
                 isPaused = await hre.Diamond.assetActionPaused(
                     Action.LIQUIDATION.toString(),
                     this.collateral.address,
-                );
-                expect(isPaused).to.equal(false);
-            });
-        });
-
-        describe("toggle actions only for listed assets", () => {
-            it("can toggle actions for listed collateral assets", async function () {
-                await executeContractCallWithSigners(
-                    hre.Multisig,
-                    hre.Diamond,
-                    "toggleAssetsPaused",
-                    [[this.collateral.address], Action.DEPOSIT, true, 0],
-                    [this.deployer, this.devTwo, this.extOne],
-                );
-
-                const isPaused = await hre.Diamond.assetActionPaused(
-                    Action.DEPOSIT.toString(),
-                    this.collateral.address,
-                );
-                expect(isPaused).to.equal(true);
-            });
-
-            it("can toggle actions for listed krAssets", async function () {
-                await executeContractCallWithSigners(
-                    hre.Multisig,
-                    hre.Diamond,
-                    "toggleAssetsPaused",
-                    [[this.krAsset.address], Action.REPAY, true, 0],
-                    [this.deployer, this.devTwo, this.extOne],
-                );
-
-                const isPaused = await hre.Diamond.assetActionPaused(
-                    Action.REPAY.toString(),
-                    this.krAsset.address,
-                );
-                expect(isPaused).to.equal(true);
-            });
-
-            it("cannot toggle actions for addresses that are not listed collateral assets or krAssets", async function () {
-                const randomAddr = hre.ethers.utils.computeAddress("0xb976778317b23a1285ec2d483eda6904d9319135b89f1d8eee9f6d2593e2665d");
-
-                await expect(executeContractCallWithSigners(
-                    hre.Multisig,
-                    hre.Diamond,
-                    "toggleAssetsPaused",
-                    [[randomAddr], Action.DEPOSIT, true, 0],
-                    [this.deployer, this.devTwo, this.extOne],
-                )).to.be.revertedWith("");
-
-                const isPaused = await hre.Diamond.assetActionPaused(
-                    Action.DEPOSIT.toString(),
-                    randomAddr,
                 );
                 expect(isPaused).to.equal(false);
             });
