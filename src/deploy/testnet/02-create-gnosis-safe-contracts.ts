e2b9729af3563639df9e0ec2ace1a10d702446ce
diofinance <diofinance@protonmail.com>
2022-10-17 18:11:05 +0200

    upstream package doesn't contain chain ID

--- src/deploy/testnet/02-create-gnosis-safe-contracts.ts
+++ src/deploy/testnet/02-create-gnosis-safe-contracts.ts
@@ -1,6 +1,17 @@
 import { DeployFunction } from "@kreskolabs/hardhat-deploy/types";
 import { getLogger } from "@kreskolabs/lib/dist/utils";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import {
+    getProxyFactoryDeployment,
+    getDefaultCallbackHandlerDeployment,
+    getCompatibilityFallbackHandlerDeployment,
+    getCreateCallDeployment,
+    getMultiSendDeployment,
+    getMultiSendCallOnlyDeployment,
+    getSignMessageLibDeployment,
+    getSafeSingletonDeployment,
+    getSafeL2SingletonDeployment
+} from "@gnosis.pm/safe-deployments";
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre;
@@ -9,82 +20,38 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     const logger = getLogger("gnosis-safe-contracts-for-tests");
 
-    await deploy("SimulateTxAccessor", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
+    // Each deployment method takes an optional DeploymentFilter as a parameter:
+    //
+    // interface DeploymentFilter {
+    //     version?: string,
+    //     released?: boolean, // Defaults to true if no filter is specified
+    //     network?: string // Chain id of the network
+    // }
 
-    await deploy("GnosisSafeProxyFactory", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
+    // const GoerliNetworkParams = { network: "420", version: "1.0.0" }
 
-    await deploy("DefaultCallbackHandler", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
+    // const proxyFactory = getProxyFactoryDeployment(GoerliNetworkParams)
+    // const defaultCallbackHandler = getDefaultCallbackHandlerDeployment(GoerliNetworkParams)
+    // const compatibilityFallbackHandler = getCompatibilityFallbackHandlerDeployment(GoerliNetworkParams)
+    // const createCallLib = getCreateCallDeployment(GoerliNetworkParams)
+    // const multiSendLib = getMultiSendDeployment(GoerliNetworkParams)
+    // const multiSendCallOnlyLib = getMultiSendCallOnlyDeployment(GoerliNetworkParams)
+    // const signMessageLib = getSignMessageLibDeployment(GoerliNetworkParams)
+    // const gnosisSafeSingleton = getSafeSingletonDeployment(GoerliNetworkParams)
+    // getSimulateTxAccesorDeployment available in latest 'main' branch but not v1.3.0 deployment
+    // const simulateTxAccessor = getSimulateTxAccesorDeployment(GoerliNetworkParams)
 
-    await deploy("CompatibilityFallbackHandler", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
+    // gnosisSafeL2Singleton: gnosisSafe version with additional events used on L2 networks
+    const gnosisSafeL2Singleton = getSafeL2SingletonDeployment({ network: "5" })
+    console.log(gnosisSafeL2Singleton)
 
-    await deploy("CreateCall", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
-
-    await deploy("MultiSend", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
-
-    await deploy("MultiSendCallOnly", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
-
-    await deploy("SignMessageLib", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
-
-    await deploy("GnosisSafe", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
-
-    await deploy("GnosisSafeL2", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
-
-    await deploy("ReentrancyTransactionGuard", {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: true,
-    });
+    // TODO: no ReentrancyTransactionGuard replacement
+    // await deploy("ReentrancyTransactionGuard", {
+    //     from: deployer,
+    //     args: [],
+    //     log: true,
+    //     deterministicDeployment: true,
+    // });
 
     logger.success("safe contracts succesfully deployed");
 };
