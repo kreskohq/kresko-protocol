3500408f786bfe2b989452a39e989060180d59fb
panukettu <panu@kresko.fi>
2022-10-18 22:58:17 +0300

    fix: pragmas

--- src/contracts/kiss/KISSConverter.sol
+++ src/contracts/kiss/KISSConverter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.14;
+pragma solidity >=0.8.14;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
