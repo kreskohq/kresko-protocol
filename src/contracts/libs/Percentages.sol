510d8df2283465eacd20bc3c79a44b92c3e1eda4
panukettu <panu@kresko.fi>
2022-11-16 21:01:04 +0200

    feat: update percentage math to newer version

--- src/contracts/libs/Percentages.sol
+++ src/contracts/libs/Percentages.sol
@@ -1,47 +1,57 @@
-// SPDX-License-Identifier: agpl-3.0
-pragma solidity >=0.8.14;
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.8.10;
 
 /**
- * @title Percentages
+ * @title PercentageMath library
  * @author Aave
  * @notice Provides functions to perform percentage calculations
  * @dev Percentages are defined by default with 2 decimals of precision (100.00).
  * The precision is indicated by PERCENTAGE_FACTOR
- * @dev Operations are rounded half up
+ * @dev Operations are rounded. If a value is >=.5, will be rounded up, otherwise rounded down.
  **/
+library PercentageMath {
+    // Maximum percentage factor (100.00%)
+    uint256 internal constant PERCENTAGE_FACTOR = 1e4;
 
-library Percentages {
-    uint256 internal constant PERCENTAGE_FACTOR = 1e4; //percentage plus two decimals
-    uint256 internal constant HALF_PERCENT = PERCENTAGE_FACTOR / 2;
+    // Half percentage factor (50.00%)
+    uint256 internal constant HALF_PERCENTAGE_FACTOR = 0.5e4;
 
     /**
-     * @dev Executes a percentage multiplication
+     * @notice Executes a percentage multiplication
+     * @dev assembly optimized for improved gas savings: https://twitter.com/transmissions11/status/1451131036377571328
      * @param value The value of which the percentage needs to be calculated
      * @param percentage The percentage of the value to be calculated
-     * @return The percentage of value
+     * @return result value percentmul percentage
      **/
-    function percentMul(uint256 value, uint256 percentage) internal pure returns (uint256) {
-        if (value == 0 || percentage == 0) {
-            return 0;
+    function percentMul(uint256 value, uint256 percentage) internal pure returns (uint256 result) {
+        // to avoid overflow, value <= (type(uint256).max - HALF_PERCENTAGE_FACTOR) / percentage
+        assembly {
+            if iszero(or(iszero(percentage), iszero(gt(value, div(sub(not(0), HALF_PERCENTAGE_FACTOR), percentage))))) {
+                revert(0, 0)
+            }
+
+            result := div(add(mul(value, percentage), HALF_PERCENTAGE_FACTOR), PERCENTAGE_FACTOR)
         }
-
-        require(value <= (type(uint256).max - HALF_PERCENT) / percentage, "Multiplication overflow");
-
-        return (value * percentage + HALF_PERCENT) / PERCENTAGE_FACTOR;
     }
 
     /**
-     * @dev Executes a percentage division
+     * @notice Executes a percentage division
+     * @dev assembly optimized for improved gas savings: https://twitter.com/transmissions11/status/1451131036377571328
      * @param value The value of which the percentage needs to be calculated
      * @param percentage The percentage of the value to be calculated
-     * @return The value divided the percentage
+     * @return result value percentdiv percentage
      **/
-    function percentDiv(uint256 value, uint256 percentage) internal pure returns (uint256) {
-        require(percentage != 0, "Division by zero");
-        uint256 halfPercentage = percentage / 2;
+    function percentDiv(uint256 value, uint256 percentage) internal pure returns (uint256 result) {
+        // to avoid overflow, value <= (type(uint256).max - halfPercentage) / PERCENTAGE_FACTOR
+        assembly {
+            if or(
+                iszero(percentage),
+                iszero(iszero(gt(value, div(sub(not(0), div(percentage, 2)), PERCENTAGE_FACTOR))))
+            ) {
+                revert(0, 0)
+            }
 
-        require(value <= (type(uint256).max - halfPercentage) / PERCENTAGE_FACTOR, "Multiplication overflow");
-
-        return (value * PERCENTAGE_FACTOR + halfPercentage) / percentage;
+            result := div(add(mul(value, PERCENTAGE_FACTOR), div(percentage, 2)), percentage)
+        }
     }
 }
