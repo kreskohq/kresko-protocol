a4b7cb9dd9c4602fe09c67097da94a4045b7a7ad
panukettu <panu@0xp.lol>
2023-04-02 10:36:52 +0300

    fix: remove typechain import

--- src/tasks/vendor/univ2-get-codehash.ts
+++ src/tasks/vendor/univ2-get-codehash.ts
@@ -1,12 +1,12 @@
-import { getLogger } from "@kreskolabs/lib";
+// import { getLogger } from "@kreskolabs/lib";
 import { task } from "hardhat/config";
-import { UniswapV2Pair__factory } from "types/typechain";
+// import { UniswapV2Pair__factory } from "types/typechain";
 import { TASK_UNIV2_GET_INIT_CODEHASH } from "../names";
 // import { bytecode } from "artifacts/src/contracts/vendor/uniswap/v2-core/UniswapV2Pair.sol/UniswapV2Pair.json";
 
 task(TASK_UNIV2_GET_INIT_CODEHASH).setAction(async function (_taskArgs, _hre) {
-    const logger = getLogger(TASK_UNIV2_GET_INIT_CODEHASH, true);
-    const INIT_CODE_HASH = hre.ethers.utils.keccak256(UniswapV2Pair__factory.bytecode);
-    logger.log(`INIT_CODE_HASH: ${INIT_CODE_HASH}`);
-    return INIT_CODE_HASH;
+    // const logger = getLogger(TASK_UNIV2_GET_INIT_CODEHASH, true);
+    // const INIT_CODE_HASH = hre.ethers.utils.keccak256(UniswapV2Pair__factory.bytecode);
+    // logger.log(`INIT_CODE_HASH: ${INIT_CODE_HASH}`);
+    // return INIT_CODE_HASH;
 });
