51ec3dd5498f26b4919fd61da5e3dabc52c06d2c
panukettu <panu@0xp.lol>
2023-08-01 20:59:47 +0300

    fix: import

--- src/tasks/vendor/univ2-get-codehash.ts
+++ src/tasks/vendor/univ2-get-codehash.ts
@@ -2,12 +2,12 @@
 import { task } from "hardhat/config";
 // import { UniswapV2Pair__factory } from "types/typechain";
 import { TASK_UNIV2_GET_INIT_CODEHASH } from "../names";
-import { getLogger } from "@kreskolabs/lib";
-import { bytecode } from "artifacts/src/contracts/vendor/uniswap/v2-core/UniswapV2Pair.sol/UniswapV2Pair.json";
+// import { getLogger } from "@kreskolabs/lib";
+// import { bytecode } from "artifacts/src/contracts/vendor/uniswap/v2-core/UniswapV2Pair.sol/UniswapV2Pair.json";
 
 task(TASK_UNIV2_GET_INIT_CODEHASH).setAction(async function (_taskArgs, _hre) {
-    const logger = getLogger(TASK_UNIV2_GET_INIT_CODEHASH, true);
-    const INIT_CODE_HASH = hre.ethers.utils.keccak256(bytecode);
-    logger.log(`INIT_CODE_HASH: ${INIT_CODE_HASH}`);
-    return INIT_CODE_HASH;
+    // const logger = getLogger(TASK_UNIV2_GET_INIT_CODEHASH, true);
+    // const INIT_CODE_HASH = hre.ethers.utils.keccak256(bytecode);
+    // logger.log(`INIT_CODE_HASH: ${INIT_CODE_HASH}`);
+    // return INIT_CODE_HASH;
 });
