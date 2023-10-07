32d5d25977574f2fb30f3dda24c485814d8d8263
panukettu <panu@0xp.lol>
2023-10-07 06:01:44 +0300

    fix: format

--- src/deploy/local/09-mint-krassets.ts
+++ src/deploy/local/09-mint-krassets.ts
@@ -19,14 +19,8 @@ const deploy: DeployFunction = async function (hre) {
   await DAI.mint(deployer.address, toBig(2_500_000_000));
   await DAI.approve(kresko.address, hre.ethers.constants.MaxUint256);
   await kresko.connect(deployer).depositCollateral(deployer.address, DAI.address, toBig(2_500_000_000));
-
-  await wrapKresko(kresko, deployer).mintKreskoAsset(
-    deployer.address,
-    (
-      await hre.getContractOrFork('KISS')
-    ).address,
-    toBig(1_200_000_000),
-  );
+  const KISS = await hre.getContractOrFork('KISS');
+  await wrapKresko(kresko, deployer).mintKreskoAsset(deployer.address, KISS.address, toBig(1_200_000_000));
 
   for (const krAsset of krAssets) {
     const asset = await hre.getContractOrFork('KreskoAsset', krAsset.symbol);
