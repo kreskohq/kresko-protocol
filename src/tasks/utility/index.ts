6a9832543c51c73fb72b053f0c1989e29d212531
panukettu <panu@0xp.lol>
2023-10-13 17:02:16 +0300

    fix: imports and file name

--- src/tasks/utility/index.ts
+++ src/tasks/utility/index.ts
@@ -3,5 +3,4 @@ export * from './create-exports';
 export * from './print-deployments';
 export * from './test-accounts';
 export * from './verify-deployments';
-export * from './write-oracles';
 export * from './write-subgraph';
