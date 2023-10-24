a3a1ee45670122be43360e60ff0cad8b49f0ad2d
panukettu <panu@0xp.lol>
2023-10-24 22:08:08 +0300

    feat: deploy state

---
+++ src/contracts/scripts/deploy/base/DeployCallbacks.s.sol
@@ -0,0 +1,181 @@
+// solhint-disable state-visibility, max-states-count, var-name-mixedcase, no-global-import, const-name-snakecase, no-empty-blocks, no-console
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {RawPrice} from "common/Types.sol";
+import {VaultAsset} from "vault/VTypes.sol";
+import {ScriptBase} from "kresko-lib/utils/ScriptBase.s.sol";
+import {console2} from "forge-std/Console2.sol";
+import {KreskoForgeUtils} from "scripts/utils/KreskoForgeUtils.s.sol";
+import {IDeployState} from "./IDeployState.sol";
+import {state} from "./DeployState.s.sol";
+import {Help, Log} from "kresko-lib/utils/Libs.sol";
+import {Enums} from "common/Constants.sol";
+
+abstract contract DeployCallbacks is IDeployState, KreskoForgeUtils {
+    function onConfigurationsCreated(
+        State storage _ctx,
+        CoreConfig memory _cfg,
+        AssetCfg memory _assetCfg,
+        UserCfg[] memory _userCfg
+    ) internal virtual {}
+
+    function onCoreContractsCreated(State storage _ctx) internal virtual {}
+
+    function onContractsCreated(State storage _ctx) internal virtual {}
+
+    function onKISSCreated(State storage _ctx, KISSInfo memory _onChainInfo) internal virtual {}
+
+    function onKrAssetAdded(State storage _ctx, KrAssetInfo memory _onChainInfo) internal virtual {}
+
+    function onVaultAssetAdded(State storage _ctx, string memory _symbol, VaultAsset memory _onChainInfo) internal virtual {}
+
+    function onExtAssetAdded(State storage _ctx, ExtAssetInfo memory _onChainInfo) internal virtual {}
+
+    function onAssetsComplete(State storage _ctx, IDeployState.AssetsOnChain memory _onChainInfo) internal virtual {}
+
+    function onDeploymentComplete(State storage _ctx) internal virtual {}
+
+    function onComplete(State storage _ctx) internal virtual {}
+}
+
+abstract contract BaseLogger is DeployCallbacks, ScriptBase {
+    using Help for *;
+    using Log for *;
+
+    function enableLogger() internal {
+        state().logEnabled = true;
+    }
+
+    function onConfigurationsCreated(
+        State storage,
+        CoreConfig memory coreCfg,
+        AssetCfg memory assetCfg,
+        UserCfg[] memory userCfg
+    ) internal override {
+        ("/* ------------------------------- Deploying -------------------------------- */").clg();
+        Log.br();
+        super.logCallers();
+        Log.br();
+        ("/* ------------------------------ Configuration ----------------------------- */").clg();
+        Log.br();
+        coreCfg.admin.clg("Admin");
+        coreCfg.seqFeed.clg("Sequencer");
+        coreCfg.council.clg("Council");
+        coreCfg.treasury.clg("Treasury");
+        coreCfg.minterMcr.pct("Minter MCR");
+        coreCfg.minterLt.pct("Minter LT");
+        coreCfg.scdpMcr.pct("SCDP MCR");
+        coreCfg.scdpLt.pct("SCDP LT");
+        coreCfg.staleTime.clg("Oracle Stale Time");
+        coreCfg.oraclePrecision.clg("Oracle Price Precision");
+        coreCfg.sdiPrecision.clg("SDI Price Precision");
+        ("-------  Assets --------").clg();
+        assetCfg.ext.length.clg("Total External Assets");
+        assetCfg.kra.length.clg("Total Kresko Assets");
+        assetCfg.vassets.length.clg("Total Vault Assets");
+        ("-------  Users --------").clg();
+        userCfg.length.clg("Total Test Users: ");
+    }
+
+    function onKISSCreated(State storage _ctx, KISSInfo memory _kiss) internal override {
+        Log.br();
+        ("/* ------------------------------ Contracts ----------------------------- */").clg();
+        Log.br();
+        address(_ctx.kresko).clg("Diamond");
+        address(_ctx.vault).clg("Vault");
+        address(_ctx.kiss).clg("KISS");
+        address(_ctx.factory).clg("Deployment Factory");
+    }
+
+    function onKrAssetAdded(State storage _ctx, KrAssetInfo memory _info) internal override {
+        RawPrice memory price = _ctx.kresko.getPushPrice(_info.addr);
+        Log.br();
+        ("/* ------------------------------ Kresko Asset ------------------------------ */").clg();
+        _info.symbol.clg("Symbol");
+        _info.addr.clg("Address");
+        ("-------  Types --------").clg();
+        _info.config.isMinterMintable.clg("Minter Mintable");
+        _info.config.isMinterCollateral.clg("Minter Collateral");
+        _info.config.isSwapMintable.clg("SCDP Swappable");
+        _info.config.isSharedCollateral.clg("SCDP Depositable");
+        ("-------  Oracle --------").clg();
+        _info.config.ticker.blg2txt("Ticker");
+        price.feed.clg("Feed");
+        uint256(price.answer).dlg("Feed Price", 8);
+        ([uint8(_info.config.oracles[0]), uint8(_info.config.oracles[1])]).clg("Oracle Types");
+        ("-------  Config --------").clg();
+        _info.config.maxDebtMinter.dlg("Minter Debt Limit", 18);
+        _info.config.maxDebtSCDP.dlg("SCDP Debt Limit", 18);
+        _info.config.kFactor.pct("kFactor");
+        _info.config.factor.pct("cFactor");
+        _info.config.openFee.pct("Minter Open Fee");
+        _info.config.closeFee.pct("Minter Close Fee");
+        _info.config.swapInFeeSCDP.pct("SCDP Swap In Fee");
+        _info.config.swapOutFeeSCDP.pct("SCDP Swap Out Fee");
+        _info.config.protocolFeeShareSCDP.pct("SCDP Protocol Fee");
+        _info.config.liqIncentiveSCDP.pct("SCDP Liquidation Incentive");
+    }
+
+    function onExtAssetAdded(State storage _ctx, ExtAssetInfo memory _info) internal override {
+        RawPrice memory price = _ctx.kresko.getPushPrice(_info.addr);
+        Log.br();
+        ("/* ----------------------------- External Asset ----------------------------- */").clg();
+        _info.symbol.clg("Symbol");
+        _info.addr.clg("Address");
+        ("-------  Types --------").clg();
+        _info.config.isMinterMintable.clg("Minter Mintable");
+        _info.config.isMinterCollateral.clg("Minter Collateral");
+        _info.config.isSwapMintable.clg("SCDP Swappable");
+        _info.config.isSharedCollateral.clg("SCDP Depositable");
+        ("-------  Oracle --------").clg();
+        _info.config.ticker.blg2txt("Ticker");
+        price.feed.clg("Feed");
+        price.answer.dlg("Feed Price", 8);
+        ([uint8(_info.config.oracles[0]), uint8(_info.config.oracles[1])]).clg("Oracle Types");
+        ("-------  Config --------").clg();
+        _info.config.kFactor.pct("kFactor");
+        _info.config.factor.pct("cFactor");
+        _info.config.liqIncentive.pct("Liquidation Incentive");
+    }
+
+    function onVaultAssetAdded(State storage _ctx, string memory _symbol, VaultAsset memory _info) internal override {
+        address assetAddr = address(_info.token);
+        Log.br();
+        ("/* ------------------------------- Vault Asset ------------------------------ */").clg();
+        _symbol.clg("Symbol");
+        assetAddr.clg("Address");
+        ("-------  Oracle --------").clg();
+        address(_info.feed).clg("Feed");
+        _ctx.vault.assetPrice(assetAddr).dlg("Price", 8);
+        _info.staleTime.clg("Stale Time");
+        ("-------  Config --------").clg();
+        _info.maxDeposits.dlg("depositLimit", _info.decimals);
+        _info.decimals.clg("decimals");
+        _info.depositFee.pct("depositFee");
+    }
+
+    function onComplete(State storage _ctx) internal override {
+        Log.br();
+        Log.sr();
+        Log.sr();
+        ("*******************  FINISHED  *******************").clg();
+        Log.sr();
+        Log.sr();
+        for (uint256 j; j < _ctx.userCfg.length; j++) {
+            Log.br();
+            Log.hr();
+            j.clg("Test User");
+            _ctx.userCfg[j].addr.clg("Address");
+            Log.hr();
+            emit Log.log_named_decimal_uint("Ether", _ctx.userCfg[j].addr.balance, 18);
+            for (uint256 i; i < _ctx.assetsOnChain.ext.length; i++) {
+                ExtAssetInfo memory asset = _ctx.assetsOnChain.ext[i];
+                uint256 balance = asset.token.balanceOf(_ctx.userCfg[j].addr);
+                balance.dlg(asset.symbol, asset.config.decimals);
+                // emit Log.log_named_decimal_uint(asset.symbol, balance, asset.config.decimals);
+            }
+        }
+        Log.hr();
+    }
+}
---
+++ src/contracts/scripts/deploy/base/IDeployState.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {IAggregatorV3} from "kresko-lib/vendor/IAggregatorV3.sol";
+import {KISS} from "kiss/KISS.sol";
+import {Enums} from "common/Constants.sol";
+import {IKreskoForgeTypes} from "scripts/utils/IKreskoForgeTypes.sol";
+import {Deployment} from "factory/DeploymentFactory.sol";
+import {Vault} from "vault/Vault.sol";
+import {MockERC20} from "mocks/MockERC20.sol";
+import {WETH9} from "kresko-lib/token/WETH9.sol";
+import {Asset} from "common/Types.sol";
+import {VaultAsset} from "vault/VTypes.sol";
+import {IERC20} from "kresko-lib/token/IERC20.sol";
+import {IKresko} from "periphery/IKresko.sol";
+import {DeploymentFactory} from "factory/DeploymentFactory.sol";
+
+/// @dev Stateful context for deployment scripts
+interface IDeployState is IKreskoForgeTypes {
+    struct State {
+        mapping(uint256 => KrAssetInfo) krAssetAt;
+        mapping(uint256 => ExtAssetInfo) extAssetAt;
+        mapping(string => address) getAddress;
+        mapping(string => Asset) getAsset;
+        mapping(string => VaultAsset) getVAsset;
+        mapping(string => IERC20) getToken;
+        mapping(string => address) getFeed;
+        mapping(string => IAggregatorV3) getVFeed;
+        mapping(string => MockERC20) getMockToken;
+        mapping(string => Deployment) getDeploy;
+        CoreConfig cfg;
+        AssetCfg assetCfg;
+        UserCfg[] userCfg;
+        AssetsOnChain assetsOnChain;
+        KISS kiss;
+        IKresko kresko;
+        Vault vault;
+        DeploymentFactory factory;
+        Deployment[] allProxies;
+        IERC20[] allTokens;
+        address[] allFeeds;
+        WETH9 weth;
+        Asset[] allAssets;
+        string[] allSymbols;
+        bytes32[] allTickers;
+        address deployer;
+        address msgSender;
+        bool logEnabled;
+    }
+
+    struct AssetsOnChain {
+        uint256 wethIndex;
+        ExtAssetInfo[] ext;
+        KrAssetInfo[] kra;
+        VaultAsset[] vassets;
+        string[] vaultSymbols;
+        uint256 extAssetCount;
+        uint256 krAssetCount;
+        uint256 vaultAssetCount;
+        KISSInfo kiss;
+    }
+
+    struct DeploymentResult {
+        AssetsOnChain assets;
+    }
+
+    struct AssetCfg {
+        uint256 wethIndex;
+        ExtAssetCfg[] ext;
+        KrAssetCfg[] kra;
+        VaultAsset[] vassets;
+        string[] vaultSymbols;
+    }
+    struct UserCfg {
+        address addr;
+        uint256[] bal;
+    }
+
+    struct KrAssetCfg {
+        string name;
+        string symbol;
+        bytes32 ticker;
+        address underlying;
+        address[2] feeds;
+        Enums.OracleType[2] oracleType;
+        AssetType identity;
+        bool setTickerFeeds;
+    }
+
+    struct ExtAssetCfg {
+        bytes32 ticker;
+        string symbol;
+        IERC20 token;
+        address[2] feeds;
+        Enums.OracleType[2] oracleType;
+        AssetType identity;
+        bool setTickerFeeds;
+    }
+}
