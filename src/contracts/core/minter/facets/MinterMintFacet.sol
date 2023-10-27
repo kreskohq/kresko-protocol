e8148a87a5afb7be87266b508b0b27c97b1b3fa2
panukettu <panu@0xp.lol>
2023-10-27 17:45:10 +0300

    feat: add market status check in scdp mints

--- src/contracts/core/minter/facets/MinterMintFacet.sol
+++ src/contracts/core/minter/facets/MinterMintFacet.sol
@@ -36,6 +36,7 @@ contract MinterMintFacet is IMinterMintFacet, Modifiers {
         Asset storage asset = cs().onlyMinterMintable(_krAsset, Enums.Action.Borrow);
 
         MinterState storage s = ms();
+
         if (!asset.marketStatus()) revert Errors.MARKET_CLOSED(Errors.id(_krAsset), asset.ticker.toString());
 
         uint256 newSupply = IKreskoAsset(_krAsset).totalSupply() + _mintAmount;
--- utils/pm2.config.js
+++ utils/pm2.config.js
@@ -14,4 +14,4 @@ module.exports = {
       script: 'just deploy-local',
     },
   ],
-};
+}
