d9539bc57cc001ccdb0726fefd5bc3b1b3b57509
panukettu <panu@0xp.lol>
2023-10-27 23:05:45 +0300

    feat: renaming facet funcs

--- src/contracts/periphery/facets/FrontendFacet.sol
+++ src/contracts/periphery/facets/FrontendFacet.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract FrontendFacet {
-    function getAccountFrontend(address _user) external view returns (uint256) {
+    function getFrontendUser(address _user) external view returns (uint256) {
         return 0;
     }
 
-    function getCommonDataFrontend(address _user) external view returns (uint256) {
+    function getFrontendProtocol() external view returns (uint256) {
         return 0;
     }
 }
