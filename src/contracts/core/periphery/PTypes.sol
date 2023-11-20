a40a2ce9874f983cd9a6a7f5f42537911adc1d59
panukettu <panu@0xp.lol>
2023-11-20 21:33:56 +0200

    fix: repack structs in periphery types to avoid viem bugs

--- src/contracts/core/periphery/PTypes.sol
+++ src/contracts/core/periphery/PTypes.sol
@@ -7,8 +7,10 @@ import {IKreskoAsset} from "kresko-asset/IKreskoAsset.sol";
 
 library PType {
     struct AssetData {
-        address addr;
+        Asset config;
         uint256 amountColl;
+        address addr;
+        string symbol;
         uint256 amountCollFees;
         uint256 valColl;
         uint256 valCollAdj;
@@ -18,8 +20,6 @@ library PType {
         uint256 valDebtAdj;
         uint256 amountSwapDeposit;
         uint256 price;
-        string symbol;
-        Asset config;
     }
 
     struct STotals {
@@ -36,19 +36,19 @@ library PType {
     }
 
     struct Protocol {
+        Gate gate;
         PAsset[] assets;
         Minter minter;
         SCDP scdp;
+        uint256 sequencerGracePeriodTime;
         uint32 staleTime;
-        uint32 sequencerGracePeriodTime;
         uint16 maxPriceDeviationPct;
         uint8 oracleDecimals;
         uint32 sequencerStartedAt;
         uint32 timestamp;
-        uint32 blockNr;
         bool isSequencerUp;
         bool safetyStateSet;
-        Gate gate;
+        uint256 blockNr;
     }
 
     struct Account {
@@ -60,11 +60,11 @@ library PType {
 
     struct Balance {
         address addr;
+        string name;
         address token;
+        string symbol;
         uint256 amount;
         uint256 val;
-        string symbol;
-        string name;
         uint8 decimals;
     }
 
@@ -76,14 +76,14 @@ library PType {
     }
 
     struct SCDP {
-        uint32 MCR;
-        uint32 LT;
-        uint32 MLR;
-        uint32 coverThreshold;
-        uint32 coverIncentive;
         SDeposit[] deposits;
         PAssetEntry[] debts;
         STotals totals;
+        uint256 coverIncentive;
+        uint32 coverThreshold;
+        uint32 MCR;
+        uint32 LT;
+        uint32 MLR;
     }
 
     struct Gate {
@@ -99,15 +99,15 @@ library PType {
     }
 
     struct PAsset {
+        Asset config;
+        uint256 tSupply;
+        IKreskoAsset.Wrapping synthwrap;
+        RawPrice priceRaw;
         address addr;
         bool isMarketOpen;
         string name;
         string symbol;
-        uint256 tSupply;
         uint256 price;
-        IKreskoAsset.Wrapping synthwrap;
-        RawPrice priceRaw;
-        Asset config;
     }
 
     struct MAccount {
@@ -134,9 +134,10 @@ library PType {
     }
 
     struct SDeposit {
+        Asset config;
+        uint256 amount;
         address addr;
         string symbol;
-        uint256 amount;
         uint256 amountSwapDeposit;
         uint256 amountFees;
         uint256 val;
@@ -145,31 +146,30 @@ library PType {
         uint128 feeIndex;
         uint128 liqIndex;
         uint256 price;
-        Asset config;
     }
 
     struct SDepositUser {
+        Asset config;
+        uint256 amount;
         address addr;
         string symbol;
-        uint256 amount;
         uint256 amountFees;
         uint256 val;
         uint256 valFees;
         uint128 feeIndex;
         uint128 liqIndex;
         uint256 price;
-        Asset config;
     }
 
     struct PAssetEntry {
+        Asset config;
+        uint256 amount;
         address addr;
         string symbol;
-        uint256 amount;
         uint256 amountAdj;
         uint256 val;
         uint256 valAdj;
         int256 index;
         uint256 price;
-        Asset config;
     }
 }
