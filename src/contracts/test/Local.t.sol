07c7af63c62c71e9a5c96747e338c282cd584fc0
panukettu <panu@0xp.lol>
2023-11-21 01:54:09 +0200

    misc: test local usdce trade to kiss

--- src/contracts/test/Local.t.sol
+++ src/contracts/test/Local.t.sol
@@ -18,16 +18,24 @@ contract NewTest is TestBase("MNEMONIC_DEVNET") {
         value = 1;
     }
 
-    // function testSomething() public {
-    //     vm.createSelectFork("localhost");
-    //     KISS kiss = KISS(0xfff7895810Dd83345c97FE90A95450d88F1c25C5);
-    //     IVault vault = IVault(0xc7B3dA534D0114DeF17400279A35137E5A649F9e);
-    //     IERC20 usdc = IERC20(0xF5A0D69303a45D71c96d8414e3591393e393C64A);
-    //     kiss.balanceOf(getAddr(0)).dlg("deplo-bal", 18);
-    //     vault.maxRedeem(address(usdc), getAddr(0)).dlg("max-redeem", 18);
-    //     (uint256 preview, uint256 fees) = vault.previewRedeem(address(usdc), 1 ether);
-    //     preview.dlg("preview", 18);
-    //     assertEq(value, 1, "val-not-eq");
-    //     assertEq(addr, address(0x123), "addr-not-eq");
-    // }
+    function testLocalThing() public {
+        vm.createSelectFork("localhost");
+        KISS kiss = KISS(0x43e53A48Ee932BbB1D09180a2bF02bBe70020449);
+        IVault vault = IVault(0x6a35d47B9139C6390bE8487836E54AB56eB4135B);
+        IERC20 usdce = IERC20(0x478c4dd5f377De1a9D1Dc5bf11457a00E9C0e7F6);
+        prank(getAddr(0));
+        uint256 allowanceToVault = usdce.allowance(getAddr(0), address(vault));
+        uint256 allowanceToKiss = usdce.allowance(getAddr(0), address(kiss));
+        allowanceToKiss.dlg("allowance-kiss", 6);
+        allowanceToVault.dlg("allowance-vault", 6);
+        // usdce.approve(address(vault), 1 ether);
+        usdce.approve(address(kiss), 1 ether);
+        usdce.balanceOf(getAddr(0)).dlg("deplo-bal", 6);
+        kiss.vaultDeposit(address(usdce), 111200, getAddr(0));
+        // vault.maxRedeem(address(usdc), getAddr(0)).dlg("max-redeem", 18);
+        // (uint256 preview, uint256 fees) = vault.previewRedeem(address(usdc), 1 ether);
+        // preview.dlg("preview", 18);
+        // assertEq(value, 1, "val-not-eq");
+        // assertEq(addr, address(0x123), "addr-not-eq");
+    }
 }
