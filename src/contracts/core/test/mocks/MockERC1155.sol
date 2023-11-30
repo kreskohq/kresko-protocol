7503be709cb7dd1d5ff3644de3c33be9cbfa5c28
panukettu <panu@0xp.lol>
2023-11-30 05:48:22 +0200

    chore: deploy uniswap and mock nfts to sepolias

--- src/contracts/core/test/mocks/MockERC1155.sol
+++ src/contracts/core/test/mocks/MockERC1155.sol
@@ -5,9 +5,21 @@ pragma solidity 0.8.21;
 import {ERC1155} from "@oz/token/ERC1155/ERC1155.sol";
 
 contract MockERC1155 is ERC1155 {
-    constructor() ERC1155("https://test") {}
+    string public name;
+    string public symbol;
+    string internal __contractUri;
+
+    constructor(string memory _name, string memory _symbol, string memory _contractUri, string memory _uri) ERC1155(_uri) {
+        name = _name;
+        symbol = _symbol;
+        __contractUri = _contractUri;
+    }
 
     function mint(address account, uint256 id, uint256 amount) external {
         _mint(account, id, amount, "");
+    }
+
+    function contractURI() public view returns (string memory) {
+        return __contractUri;
     }
 }
---
+++ src/contracts/scripts/utils/UniV3.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IPoolV3 {
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        int256 amountSpecified,
+        uint160 sqrtPriceLimitX96
+    ) external returns (int256 amount0, int256 amount1);
+
+    function initialize(uint160 sqrtPriceX96) external;
+}
+
+// calculate sqrtPrice
+// sqrtPriceX96 = sqrt( (reserve1/reserve0) * 2^96 )
+
+interface IFactoryV3 {
+    function createPool(address tokenA, address tokenB, uint24 fee) external returns (address pool);
+
+    function owner() external view returns (address);
+}
+
+interface INFTManager {
+    struct MintParams {
+        address token0;
+        address token1;
+        uint24 fee;
+        int24 tickLower;
+        int24 tickUpper;
+        uint256 amount0Desired;
+        uint256 amount1Desired;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        address recipient;
+        uint256 deadline;
+    }
+
+    /// @notice Creates a new position wrapped in a NFT
+    /// @dev Call this when the pool does exist and is initialized. Note that if the pool is created but not initialized
+    /// a method does not exist, i.e. the pool is assumed to be initialized.
+    /// @param params The params necessary to mint a position, encoded as `MintParams` in calldata
+    /// @return tokenId The ID of the token that represents the minted position
+    /// @return liquidity The amount of liquidity for this position
+    /// @return amount0 The amount of token0
+    /// @return amount1 The amount of token1
+    function mint(
+        MintParams calldata params
+    ) external payable returns (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1);
+
+    /// @notice Burns a token ID, which deletes it from the NFT contract. The token must have 0 liquidity and all tokens
+    /// must be collected first.
+    /// @param tokenId The ID of the token that is being burned
+    function burn(uint256 tokenId) external payable;
+
+    function createAndInitializePoolIfNecessary(
+        address tokenA,
+        address tokenB,
+        uint24 fee,
+        uint160 sqrtPriceX96
+    ) external returns (address pool);
+
+    struct IncreaseLiquidityParams {
+        uint256 tokenId;
+        uint256 amount0Desired;
+        uint256 amount1Desired;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        uint256 deadline;
+    }
+
+    /// @notice Increases the amount of liquidity in a position, with tokens paid by the `msg.sender`
+    /// @param params tokenId The ID of the token for which liquidity is being increased,
+    /// amount0Desired The desired amount of token0 to be spent,
+    /// amount1Desired The desired amount of token1 to be spent,
+    /// amount0Min The minimum amount of token0 to spend, which serves as a slippage check,
+    /// amount1Min The minimum amount of token1 to spend, which serves as a slippage check,
+    /// deadline The time by which the transaction must be included to effect the change
+    /// @return liquidity The new liquidity amount as a result of the increase
+    /// @return amount0 The amount of token0 to acheive resulting liquidity
+    /// @return amount1 The amount of token1 to acheive resulting liquidity
+    function increaseLiquidity(
+        IncreaseLiquidityParams calldata params
+    ) external payable returns (uint128 liquidity, uint256 amount0, uint256 amount1);
+}
