4629265416013c3dc066d5da27686db978c26c76
fxp3 <dev@0xp.lol>
2024-02-07 02:24:08 +0200

    feat: allow getting push price data

--- src/contracts/scripts/deploy/DeployFuncs.s.sol
+++ src/contracts/scripts/deploy/DeployFuncs.s.sol
@@ -24,6 +24,29 @@ function getFacetsAndSelectors() returns (string[] memory files, bytes[] memory 
     return (files, facets, selectors);
 }
 
+function getFacetsAndSelectors(
+    string memory artifact
+) returns (string[] memory files, bytes[] memory facets, bytes4[][] memory selectors) {
+    string[] memory cmd = new string[](2);
+    cmd[0] = "./utils/getBytesAndSelectors.sh";
+    cmd[1] = string.concat("./src/contracts/core/**/facets/", artifact, ".sol");
+
+    (files, selectors) = abi.decode(vmFFI.ffi(cmd), (string[], bytes4[][]));
+    facets = new bytes[](selectors.length);
+
+    for (uint256 i; i < files.length; ) {
+        (, bytes memory getCodeResult) = address(vmFFI).call(
+            abi.encodeWithSignature("getCode(string)", string.concat(files[i], ".sol:", files[i]))
+        );
+        facets[i] = abi.decode(getCodeResult, (bytes));
+        unchecked {
+            i++;
+        }
+    }
+    require(facets.length == selectors.length, "Facets and selectors length mismatch");
+    return (files, facets, selectors);
+}
+
 function create1(bytes memory bytecode) returns (address location) {
     assembly {
         location := create(0, add(bytecode, 0x20), mload(bytecode))
